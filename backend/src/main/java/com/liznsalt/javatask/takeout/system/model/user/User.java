ee0776513ba011e19c5f2326c20ad35c191b24b4
Veevee3124 <levee684@gmail.com>
2025-05-16 17:17:15 -0700

    Delete backend directory

--- backend/src/main/java/com/liznsalt/javatask/takeout/system/model/user/User.java
+++
@@ -1,9 +0,0 @@
-package com.liznsalt.javatask.takeout.system.model.user;
-
-public interface User {
-    Integer getId();
-    String getUsername();
-    String getPassword();
-    String getTelephone();
-    String getAddress();
-}
